10337ee88d9f290dcea0ae243b88cffe72f01609
Clicks <clicks@Progressives-MacBook-Pro.local>
2020-07-05 21:14:24 +0100

    2

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0e8bf45d24322eb0c378";
+/******/ 	var hotCurrentHash = "623aae8c9edfd9e3f3d9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import { Container, Row, Col, Button, Alert } from 'reactstrap';
+import { faHeart, faCommentAlt, faCalendar, faClock } from '@fortawesome/free-regular-svg-icons';
+import { faShare, faShareAlt, faShieldAlt, faShieldVirus, faSearchLocation, faMapMarked, faMapMarkedAlt, faMapPin, faMapMarker, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import Icon from '../components/Icon';
+import ShareModal from '../components/ShareModal';
+import DownloadBtns from '../components/DownloadBtns';
+import Avatar from 'react-avatar';
+import Rave from 'react-flutterwave-rave'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import JoinModal from '../components/JoinModal';
+import Share from '../components/Share';
+import axios from '../utils/axios';
+
+
+function Home() {
+
+
+  return (
+    <div>
+
+
+
+
+    </div>
+  )
+}
+
+
+export default Home
